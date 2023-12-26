257fdd99ad4df6793e89d3d5bdb31d05a1e2919f
Ismail <141173099+alcode111@users.noreply.github.com>
2023-12-26 15:55:21 +0100

    Add logic to keep data through refreshes and a few comments

--- jscript.js
+++ jscript.js
@@ -5,16 +5,33 @@ const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 
+// Keeping the data after refreshes
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+// SAVE INPUT button logic
+
 inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     render(myLeads)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
 })
 
+// DELETE ALL button logic
+
 deleteBtn.addEventListener('dblclick', function() {
+    localStorage.clear()
     myLeads = []
     render(myLeads)
 })
+
+// Function to display the leads in list items
 
 function render(leads) {
     listItems = ""
@@ -28,4 +45,3 @@ function render(leads) {
     }
     ulEl.innerHTML = listItems
 }
-
